1f23bc77c568fa0bd00d4bafbf06bd0709973001
jenamoradoc <davidenamorado09@gmail.com>
2021-04-06 00:09:17 -0300

    inicial base for groway landing

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./styles/main.scss";
+
+import Home from "./components/Home/Home";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Home extends React.Component {
+	state = {
+		num: 0,
+	};
+
+	render() {
+		return (
+			<div>
+               <p>Hola desde Home</p>
+			</div>
+		);
+	}
+}
+
+export default Home;
