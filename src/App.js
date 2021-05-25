96f83792b9aea90ec3627289140fc42e2e507204
jenamoradoc <davidenamorado09@gmail.com>
2021-05-25 13:46:38 -0300

    refactor template

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import './styles/main.scss';
+import './index.scss';
 
 import Home from "./componentes/Home";
 
--- src/componentes/Home.js
+++ src/componentes/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
+import Header from "./Header/Header";
+import Main from "./Main/Main";
+import Footer from "./layout/Footer/Footer";
 
 class Home extends React.Component {
 	state = {
