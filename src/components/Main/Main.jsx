eba97c8711b52aefce84e856b91d14ebffdb3871
joel ibaceta <mail@joelibaceta.com>
2021-10-04 01:53:18 -0500

    update styles

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -13,7 +13,7 @@ class Main extends React.Component {
         <section id="Convierte_ideas" className="block_grey">
           <div className="convierte_ideas u-space-pading--medium">
             <div className="col-1-of-2 convierte_ideas__text">
-              <h1 className="convierte_ideas__text--title">Convierte <span className="u-text--rose">ideas</span> en <span className="u-text--yellow">Productos</span> con <span className="u-text--rose">Potencial</span></h1>
+              <h1 className="convierte_ideas__text--title">Convierte <span className="u-text--rose">ideas</span><br/> en <span className="u-text--yellow">Productos</span><br/>con <span className="u-text--rose">Potencial</span></h1>
               <p className="convierte_ideas__text--subtitle">Una buena idea es el punto de partida en el camino para lograr un producto exitoso y este
                 debe ser llevada al mercado lo mas rapido posible para validar nuestras hipótesis e iniciar 
                 el proceso de ajuste y mejora continua para acercarnos cada vez mas al producto que nuestros 
@@ -41,11 +41,13 @@ class Main extends React.Component {
         <section id="tu_crecimiento" className="block_grey">
         <div className="trabajemos_juntos">
             <div className="col-1-of-2 trabajemos_juntos__text u-margin-bottom-small">
-              <h1 className="trabajemos_juntos__text--title"><span className="u-text--rose">Acompañamos</span> tu <span className="u-text--yellow">Crecimiento</span></h1>
-              <p className="trabajemos_juntos__text--subtitle">Una buena idea es el punto de partida en el camino para lograr un producto exitoso y este
-                debe ser llevada al mercado lo mas rapido posible para validar nuestras hipótesis e iniciar 
-                el proceso de ajuste y mejora continua para acercarnos cada vez mas al producto que nuestros 
-                usuarios amaran.</p>
+              <div>
+                <h1 className="trabajemos_juntos__text--title"><span className="u-text--rose">Acompañamos</span> tu <span className="u-text--yellow">Crecimiento</span></h1>
+                <p className="trabajemos_juntos__text--subtitle">Una buena idea es el punto de partida en el camino para lograr un producto exitoso y este
+                  debe ser llevada al mercado lo mas rapido posible para validar nuestras hipótesis e iniciar 
+                  el proceso de ajuste y mejora continua para acercarnos cada vez mas al producto que nuestros 
+                  usuarios amaran.</p>
+              </div>
             </div>
             <div className="col-1-of-2 trabajemos_juntos__img u-space-pading--medium">
               <img src={Crecimiento} alt="Crecimiento" />
