d518a9c7c45ce84d9f11fe88bdc804dd19c93a3c
joel ibaceta <mail@joelibaceta.com>
2022-09-30 17:42:06 +0900

    update design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,11 +12,7 @@ class Header extends React.Component {
 		return (
 			<div class="block_white blockMain" >
 				<header id="Header" class="header">
-					<div class="custom-shape-divider-bottom-1664522860">
-							<svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
-									<path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" class="shape-fill"></path>
-							</svg>
-					</div>
+				
 					<div class="col-1-of-2 header__logo">
 						<img src={Logo} alt="Logo Groway" />
 					</div>
@@ -30,6 +26,7 @@ class Header extends React.Component {
 						</div>
 					</div>
 				</header>
+				
 			</div>
 		);
 	}
